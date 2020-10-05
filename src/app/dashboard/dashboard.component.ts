ab9e32de5012306a59b2552cace54cb0f1264664
Jim Li <lij42@airproducts.com>
2020-10-05 16:32:18 +0800

    Login flow between SAP site and IDP successfully running(with some issues in callback when login completed).

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +10,7 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private _authService: AuthService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -18,5 +19,9 @@ export class DashboardComponent implements OnInit {
   getHeroes(): void{
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5));
   };
+
+  public login = () => {
+    this._authService.login();
+  }
 
 }
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,6 @@
+export class Constants {
+    public static apiRoot = "https://localhost:5001/api";
+    public static clientRoot = "http://localhost:4200";
+    public static idpAuthority = "https://dev-api.airproducts.com/oauth"
+    public static clientId = "a1d34623-b7a7-4cd7-970e-418f1c63af03";
+}
